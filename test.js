4b0fcca3bbd04f41e0ed38d93514dd802d0fe93e
Andre <66816617+Andre-Ge@users.noreply.github.com>
2020-06-12 17:25:31 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
 
+var apklama = 'https://ouo.io/b5g3qx';
+var apkbaru = 'http://adf.ly/1kE34v';
+function updateme()
+{
+var text = document.getElementsByTagName('body')[0].innerHTML;
+text = replaceAll(text,apklama,apkbaru);
+document.getElementsByTagName('body')[0].innerHTML = text;
+}
+function replaceAll(txt, replace, with_this) {
+return txt.replace(new RegExp(replace, 'g'),with_this);
+
+}
+setTimeout('updateme()',100);
