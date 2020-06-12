55a048247951a2cd6701838704bb03b43f3b15c8
Andre-Ge <66816617+Andre-Ge@users.noreply.github.com>
2020-06-12 12:55:55 +0700

    Add files via upload

---
+++ link-apk-blog.js
@@ -0,0 +1,13 @@
+var apklama = 'https://ouo.io/b5g3qx';
+var apkbaru = 'http://j.gs/EFxr';
+function updateme()
+{
+var text = document.getElementsByTagName('body')[0].innerHTML;
+text = replaceAll(text,apklama,apkbaru);
+document.getElementsByTagName('body')[0].innerHTML = text;
+}
+function replaceAll(txt, replace, with_this) {
+return txt.replace(new RegExp(replace, 'g'),with_this);
+
+}
+setTimeout('updateme()',100);
